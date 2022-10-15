b40d203d8352df17f72837c9757029e64780a3b7
Kingsley Nweye <etonwana@yahoo.com>
2022-10-15 17:28:25 -0500

    refactored for readability

--- workflow/download.sh
+++ workflow/download.sh
@@ -10,7 +10,16 @@ IFS='|'
     read
     while read -r neighborhood_id filters
     do
-        python -m doe_xstock -d $DOE_XSTOCK_DATABASE_FILEPATH dataset $DOE_XSTOCK_INSERT_DATASET_TYPE $DOE_XSTOCK_INSERT_WEATHER_DATA $DOE_XSTOCK_INSERT_YEAR_OF_PUBLICATION $DOE_XSTOCK_INSERT_RELEASE -f $filters insert || exit 1
+        python -m doe_xstock \
+            -d $DOE_XSTOCK_DATABASE_FILEPATH \
+                dataset \
+                    $DOE_XSTOCK_INSERT_DATASET_TYPE \
+                        $DOE_XSTOCK_INSERT_WEATHER_DATA \
+                            $DOE_XSTOCK_INSERT_YEAR_OF_PUBLICATION \
+                                $DOE_XSTOCK_INSERT_RELEASE \
+                                    -f $filters \
+                                        insert \
+                                            || exit 1
     done
 } < $DOE_XSTOCK_INSERT_FILTERS_FILEPATH
 IFS=$OLDIFS
--- workflow/metadata_clustering.sh
+++ workflow/metadata_clustering.sh
@@ -10,7 +10,21 @@ IFS='|'
     read
     while read -r neighborhood_id filters
     do
-        python -m doe_xstock -d $DOE_XSTOCK_DATABASE_FILEPATH -t $DOE_XSTOCK_DATA_DIRECTORY -g $DOE_XSTOCK_FIGURE_DIRECTORY dataset $DOE_XSTOCK_INSERT_DATASET_TYPE $DOE_XSTOCK_INSERT_WEATHER_DATA $DOE_XSTOCK_INSERT_YEAR_OF_PUBLICATION $DOE_XSTOCK_INSERT_RELEASE -f $filters metadata_clustering $neighborhood_id $DOE_XSTOCK_MAXIMUM_N_CLUSTERS -c $DOE_XSTOCK_NEIGHBORHOOD_SIZE|| exit 1
+        python -m doe_xstock
+            -d $DOE_XSTOCK_DATABASE_FILEPATH \
+                -t $DOE_XSTOCK_DATA_DIRECTORY \
+                    -g $DOE_XSTOCK_FIGURE_DIRECTORY \
+                        dataset \
+                            $DOE_XSTOCK_INSERT_DATASET_TYPE \
+                                $DOE_XSTOCK_INSERT_WEATHER_DATA \
+                                    $DOE_XSTOCK_INSERT_YEAR_OF_PUBLICATION \
+                                        $DOE_XSTOCK_INSERT_RELEASE \
+                                            -f $filters \
+                                                metadata_clustering \
+                                                    $neighborhood_id \
+                                                        $DOE_XSTOCK_MAXIMUM_N_CLUSTERS \
+                                                            -c $DOE_XSTOCK_NEIGHBORHOOD_SIZE \
+                                                                || exit 1
     done
 } < $DOE_XSTOCK_INSERT_FILTERS_FILEPATH
 IFS=$OLDIFS
