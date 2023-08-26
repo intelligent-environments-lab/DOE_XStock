cb5d3a84b4a975ec9597b34e5df4bfb62c48abc5
Kingsley Nweye <etonwana@yahoo.com>
2023-08-25 22:16:11 -0500

    refactored

--- analysis/queries.sql
+++ analysis/queries.sql
@@ -1,10 +1,12 @@
--- SELECT osm FROM model WHERE metadata_id = 494;
-
 -- PRAGMA foreign_keys = ON;
 -- DELETE FROM energyplus_simulation;
 -- DELETE FROM energyplus_simulation_error;
 -- DELETE FROM static_lstm_train_data;
 -- DELETE FROM dynamic_lstm_train_data;
+-- SELECT * FROM energyplus_simulation_error;
+-- SELECT osm FROM model WHERE metadata_id = 494;
+
+-- SELECT * FROM metadata_clustering where id IN (SELECT clustering_id FROM optimal_metadata_clustering);
 
 -- SELECT 
 --     bldg_id,
@@ -15,10 +17,7 @@
 -- FROM dynamic_lstm_train_data_thermal_comfort_summary 
 -- WHERE 
 --     timestep_resolution = 'monthly'
---     AND metadata_id IN (SELECT metadata_id FROM thermal_zone_count WHERE value = 1)
 -- ORDER BY one_degree_partial_unmet DESC;
-
-
 
 -- SELECT * FROM dynamic_lstm_train_data_multi_resolution_summary WHERE timestep_resolution = 'yearly' AND in_resstock_county_id = 'TX, Travis County' ORDER BY maximum_partial_setpoint_absolute_difference DESC;
 
