756399189b38005c46b114d9563b9f9db8e82397
Kingsley Nweye <etonwana@yahoo.com>
2024-02-13 22:00:10 -0600

    deleted

--- doe_xstock/misc/queries/get_design_load_temperature.sql
+++
@@ -1,7 +0,0 @@
-SELECT
-    r.TimeIndex AS timestep,
-    SUM(r.Value) AS value
-FROM weighted_variable r
-LEFT JOIN ReportDataDictionary d ON d.ReportDataDictionaryIndex = r.ReportDataDictionaryIndex
-WHERE d.Name IN ('Zone Air Temperature')
-GROUP BY r.TimeIndex
