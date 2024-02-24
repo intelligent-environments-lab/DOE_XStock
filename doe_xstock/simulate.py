9488d525f8b4de9f95f1864b94b4ef1af6494207
Kingsley Nweye <etonwana@yahoo.com>
2024-02-23 18:15:05 -0600

    added code to run ideal load simulations

--- doe_xstock/simulate.py
+++ doe_xstock/simulate.py
@@ -4,7 +4,7 @@ from pathlib import Path
 import re
 from typing import Mapping, List, Union
 from eppy.modeleditor import IDF
-from eppy.runner.run_functions import runIDFs
+from eppy.runner.run_functions import EnergyPlusRunError, runIDFs
 from openstudio import energyplus, osversion, openstudiomodelcore
 import pandas as pd
 from doe_xstock.database import SQLiteDatabase
@@ -194,21 +194,144 @@ class EnergyPlusSimulator:
 
         for simulator in simulators:
             os.makedirs(simulator.output_directory, exist_ok=True)
+            idf = simulator.preprocess_idf_for_simulation()
+            simulator.__write_idf(idf.idfstr())
             simulator.__write_epw()
-            idf = simulator.get_idf_object(weather=simulator.epw_filepath)
             kwargs = simulator.get_run_kwargs()
-            simulator.__write_idf()
             runs.append([idf, kwargs])
         
         runIDFs(runs, max_workers)
 
     def simulate(self, **run_kwargs):
         os.makedirs(self.output_directory, exist_ok=True)
-        self.__write_epw()
-        self.__write_idf()
         run_kwargs = self.get_run_kwargs(**run_kwargs if run_kwargs is not None else {})
-        idf = self.get_idf_object(epw=self.epw_filepath) 
+        idf = self.preprocess_idf_for_simulation()
+        self.__write_idf(idf.idfstr())
+        self.__write_epw()
         idf.run(**run_kwargs)
+
+    def get_error(self) -> str:
+        filepath = os.path.join(self.output_directory, f'{self.simulation_id}.err')
+
+        with open(filepath, 'r') as f:
+            error = f.read()
+
+        return error
+
+    def get_run_kwargs(self, **kwargs) -> Mapping[str, Union[bool, str]]:
+        idf = self.get_idf_object()
+        idf_version = idf.idfobjects['version'][0].Version_Identifier.split('.')
+        idf_version.extend([0] * (3 - len(idf_version)))
+        idf_version_str = '-'.join([str(item) for item in idf_version])
+        options = {
+            'ep_version': idf_version_str,
+            'output_prefix': str(self.simulation_id),
+            'output_suffix': 'C',
+            'output_directory': str(self.output_directory),
+            'readvars': True,
+            'expandobjects': True,
+            'verbose': 'q',
+        }
+        options = {**options, **kwargs}
+
+        return options
+
+    def __write_epw(self):
+        filepath = os.path.join(self.output_directory, 'weather.epw')
+
+        with open(filepath, 'w') as f:
+            f.write(self.epw)
+        
+        self.__epw_filepath = filepath
+
+    def __write_idf(self, idf: str):
+        filepath = os.path.join(self.output_directory,f'{self.simulation_id}.idf')
+
+        with open(filepath, 'w') as f:
+            f.write(idf)
+
+    def preprocess_idf_for_simulation(self) -> IDF:
+        return self.get_idf_object(self.epw)
+
+    def get_idf_object(self, epw: str = None) -> IDF:
+        return IDF(StringIO(self.idf), epw)
+    
+class EndUseLoadProfilesEnergyPlusSimulator(EnergyPlusSimulator):
+    __DEFAULT_SCHEDULES_FILEPATH = 'schedules.csv'
+
+    def __init__(
+            self, idd_filepath: Union[Path, str], model: Union[Path, str], epw: Union[Path, str], schedules_filepath: Union[Path, str] = None, output_variables: List[str] = None, osm: bool = None, ideal_loads: bool = None, edit_ems: bool = None, simulation_id: str = None, 
+            output_directory: Union[Path, str] = None
+    ):
+        self.__ideal_loads = False if ideal_loads is None else ideal_loads
+        idf = self.__set_idf(model, osm=osm)
+        super().__init__(idd_filepath, idf, epw, simulation_id, output_directory)
+        self.schedules_filepath = schedules_filepath
+        self.output_variables = output_variables
+        self.edit_ems = edit_ems
+    
+    @property
+    def schedules_filepath(self) -> Union[Path, str]:
+        return self.__schedules_filepath
+    
+    @property
+    def output_variables(self) -> List[str]:
+        return self.__output_variables
+    
+    @property
+    def edit_ems(self) -> bool:
+        return self.__edit_ems
+    
+    @schedules_filepath.setter
+    def schedules_filepath(self, value: Union[Path, str]):
+        self.__schedules_filepath = self.__DEFAULT_SCHEDULES_FILEPATH if value is None else value
+
+    @output_variables.setter
+    def output_variables(self, value: List[str]):
+        self.__output_variables = self.get_default_simulation_output_variables() if value is None else value
+
+    @edit_ems.setter
+    def edit_ems(self, value: bool):
+        self.__edit_ems = True if value is None else value
+
+    def simulate(self, **run_kwargs):
+        found_objects = True
+
+        while True:
+            try:
+                super().simulate(**run_kwargs)
+                break
+
+            except EnergyPlusRunError as e:
+                if self.__ideal_loads:
+                    try:
+                        found_objects = self.__fix_ideal_load_ems_errors(found_objects, patterns=run_kwargs.get('ems_patterns'))
+                    
+                    except Exception as e:
+                        raise e
+
+                else:
+                    raise e
+    
+    def __fix_ideal_load_ems_errors(self, found_objects: bool, patterns: List[str] = None):
+        assert (self.has_ems_input_error() or self.has_ems_program_error())
+        assert found_objects
+        
+        try:
+            removed_objects = self.remove_ems_objs_in_error(patterns=patterns)
+            
+            if self.edit_ems:
+                edited_objects = self.redefine_ems_program_in_line_error()
+            
+            else:
+                removed_objects = {**removed_objects, **self.remove_ems_program_objs_in_line_error()}
+
+            found_objects = len(removed_objects) + len(edited_objects) > 0
+        
+        except Exception as e:
+            raise e
+        
+        return found_objects
 
     def remove_ems_objs_in_error(self, patterns: List[str] = None) -> Mapping[str, List[str]]:
         default_patterns = [
@@ -309,46 +432,105 @@ class EnergyPlusSimulator:
         error = self.get_error()
 
         return len([re.findall(p, error) for p in patterns]) > 0
+    
+    def preprocess_idf_for_simulation(self) -> IDF:
+        idf = self.get_idf_object(self.epw)
 
-    def get_error(self) -> str:
-        filepath = os.path.join(self.output_directory, f'{self.simulation_id}.err')
+        # remove daylight savings definition
+        idf.idfobjects['RunPeriodControl:DaylightSavingTime'] = []
 
-        with open(filepath, 'r') as f:
-            error = f.read()
-
-        return error
-
-    def get_run_kwargs(self, **kwargs) -> Mapping[str, Union[bool, str]]:
-        idf = self.get_idf_object()
-        idf_version = idf.idfobjects['version'][0].Version_Identifier.split('.')
-        idf_version.extend([0] * (3 - len(idf_version)))
-        idf_version_str = '-'.join([str(item) for item in idf_version])
-        options = {
-            'ep_version': idf_version_str,
-            'output_prefix': str(self.simulation_id),
-            'output_suffix': 'C',
-            'output_directory': str(self.output_directory),
-            'readvars': True,
-            'expandobjects': True,
-            'verbose': 'q',
-        }
-        options = {**options, **kwargs}
-
-        return options
-
-    def __write_epw(self):
-        filepath = os.path.join(self.output_directory, 'weather.epw')
-
-        with open(filepath, 'w') as f:
-            f.write(self.epw)
+        # set schedules filepath in model
+        for obj in idf.idfobjects['Schedule:File']:
+            if obj.File_Name == self.__DEFAULT_SCHEDULES_FILEPATH:
+                obj.File_Name = self.schedules_filepath
+            
+            else:
+                continue
         
-        self.__epw_filepath = filepath
+        # set output variables
+        for output_variable in self.output_variables:
+            obj = idf.newidfobject('Output:Variable')
+            obj.Variable_Name = output_variable
+            obj.Reporting_Frequency = 'Timestep'
 
-    def __write_idf(self):
-        filepath = os.path.join(self.output_directory,f'{self.simulation_id}.idf')
+        return idf
+    
+    def get_default_simulation_output_variables(self) -> List[str]:
+        default_output_variables = []
 
-        with open(filepath, 'w') as f:
-            f.write(self.idf)
+        # weather
+        default_output_variables += [
+            'Site Diffuse Solar Radiation Rate per Area', 
+            'Site Direct Solar Radiation Rate per Area', 
+            'Site Outdoor Air Drybulb Temperature', 
+            'Site Outdoor Air Relative Humidity', 
+        ]
 
-    def get_idf_object(self, epw: str = None) -> IDF:
-        return IDF(StringIO(self.idf), epw)
+        # electric equipment
+        default_output_variables += [
+            'Zone Electric Equipment Electricity Rate', 
+            'Zone Lights Electricity Rate', 
+        ]
+
+        # mechanical hvac
+        default_output_variables += [
+            'Air System Total Cooling Energy', 
+            'Air System Total Heating Energy', 
+            'Zone Air System Sensible Cooling Rate', 
+            'Zone Air System Sensible Heating Rate', 
+            'Zone Predicted Sensible Load to Setpoint Heat Transfer Rate',
+        ]
+
+        # ideal loads system
+        default_output_variables += [
+            'Zone Ideal Loads Zone Sensible Cooling Rate', 
+            'Zone Ideal Loads Zone Sensible Heating Rate', 
+        ]
+
+        # other equipment
+        default_output_variables += [
+            'Other Equipment Convective Heating Rate', 
+            'Other Equipment Convective Heating Energy'
+        ]
+
+        # dhw
+        default_output_variables += [
+            'Water Use Equipment Heating Rate', 
+        ]
+
+        # ieq
+        default_output_variables += [
+            'Zone Air Relative Humidity', 
+            'Zone Air Temperature', 
+            'Zone Thermostat Cooling Setpoint Temperature', 
+            'Zone Thermostat Heating Setpoint Temperature', 
+        ]
+
+        # occupancy
+        default_output_variables += [
+            'Zone People Occupant Count', 
+        ]
+
+        return default_output_variables
+    
+    def __set_idf(self, model: Union[Path, str], osm: bool = None) -> str:
+        osm = False if osm is None else osm
+
+        if osm:
+            osm_model = OpenStudioModelEditor(model)
+
+            if self.__ideal_loads:
+                osm_model.use_ideal_loads_air_system()
+
+            else:
+                pass
+
+            idf = osm_model.forward_translate()
+
+        elif self.__ideal_loads:
+            raise Exception('To simulate ideal loads, model parsed at intialization must be an OpenStudio model and osm must be set to True')
+        
+        else:
+            idf = model
+
+        return idf
